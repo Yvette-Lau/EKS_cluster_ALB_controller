0f8f6b00823b681cd054021c53641beda04da657
Yvette-Lau <lauyatting9598@gmail.com>
2023-05-11 14:30:23 -0300

    helm lb controller setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EKS_cluster_ALB_controller
 Using Terraform modules to setup eks cluster with alb controller
+
+```
+# terraform init
+# terraform plan
+# terraform apply -auto-approve
+# aws eks update-kubeconfig --name my-eks --region us-east-1
+```
--- terraform/eks.tf
+++ terraform/eks.tf
@@ -55,35 +55,35 @@ module "eks" {
     Environment = "staging"
   }
 
-  # manage_aws_auth_configmap = true
-  # aws_auth_roles = [
-  #   {
-  #     rolearn  = module.eks_admins_iam_role.iam_role_arn
-  #     username = module.eks_admins_iam_role.iam_role_name
-  #     groups   = ["system:masters"]
-  #   },
-  # ]
+  manage_aws_auth_configmap = true
+  aws_auth_roles = [
+    {
+      rolearn  = module.eks_admins_iam_role.iam_role_arn
+      username = module.eks_admins_iam_role.iam_role_name
+      groups   = ["system:masters"]
+    },
+  ]
 
 }
 
-# # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
-# data "aws_eks_cluster" "default" {
-#   name = module.eks.cluster_id
+
+data "aws_eks_cluster" "default" {
+  name = module.eks.cluster_id
   
-# }
+}
 
-# data "aws_eks_cluster_auth" "default" {
-#   name = module.eks.cluster_id
-# }
+data "aws_eks_cluster_auth" "default" {
+  name = module.eks.cluster_id
+}
 
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.default.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-#   # token                  = data.aws_eks_cluster_auth.default.token
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  # token                  = data.aws_eks_cluster_auth.default.token
 
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
-#     command     = "aws"
-#   }
-# }
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    command     = "aws"
+  }
+}
